bbe16a2911222951d35fe2282f2ecf9c8b29b4f0
tinanwlin <tinanwlin@gmail.com>
2019-06-23 15:59:07 -0700

    create product and summary page without any function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
+import Summary from './components/Summary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Route exact path='/' component={Home} />
+        <Route path='/products' component={Products} />
+        <Route path='/summary' component={Summary} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Summary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Summary extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Product Summary:</h2>
+      </div>
+    )
+  }
+}
+
+export default Summary;
